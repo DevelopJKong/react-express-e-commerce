519e2f956b19e3e6190ea685724967b5d036975f
DevelopJKong <ybaram40@gmail.com>
2022-04-20 10:33:03 +0900

    Login & Register page 완료 & Cart page 구현중 [2:11:40 coding...]

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,19 +1,31 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const Router = () => {
   return (
     <BrowserRouter>
-    <Switch>
-      <Route path="/list">
-          <ProductList/>
-      </Route>
-      <Route path="/">
+      <Switch>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Router path="/login">
+          <Login />
+        </Router>
+        <Route path="/list/:id">
+          <Product />
+        </Route>
+        <Route path="/list">
+          <ProductList />
+        </Route>    
+        <Route path="/">
           <Home />
-      </Route>
-    </Switch>
-  </BrowserRouter>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
